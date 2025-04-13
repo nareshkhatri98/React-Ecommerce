747f1e6f4cbcffe9a9ffe3d9611dfde1a4d6609f
Naresh Khatri <nareshkhatri9810@gmail.com>
2025-04-13 16:50:38 +0545

    refactor TopSeller component; enhance UI and implement follow/unfollow functionality for authors

--- src/components/TopSeller.tsx
+++ src/components/TopSeller.tsx
@@ -15,9 +15,9 @@ const TopSeller = () => {
         const response = await fetch("https://randomuser.me/api/?results=5");
         const data = await response.json();
 
-        const authorData: Author[] = data.results.map((user: any) => ({
+        const authorData: Author[] = data.results.map((user:any) => ({
           name: `${user.name.first} ${user.name.last}`,
-          isFollowing: false,
+          isFollowing: false, 
           image: user.picture.medium,
         }));
 
@@ -29,10 +29,23 @@ const TopSeller = () => {
 
     fetchData();
   }, []);
-
+ const  handleFollowed = (index:number)=>{
+  setAuthors(prevAuthor=>prevAuthor.map((author,i)=>i ===index?{...author, isFollowing:!author.isFollowing}:author))
+ }
   return (
-    <div>
-      <h2>Top Sellers</h2>
+    <div className="bg-white p-5 mx-5 mt-[5rem]  border w-[23rem] rounded  ">
+      <h2 className="text-xl font-bold mb-5">Top Sellers</h2> 
+         <ul>
+          {authors.map((author,index)=>(
+            <li key={ index} className="flex items-center justify-between mb-4">
+              <section className="flex items-center justify-center">
+                <img src={author.image} alt={author.name} className="w-[25%] h-[25%] rounded-full" />
+                <span className="ml-4">{author.name}</span>
+              </section>
+              <button onClick={()=>handleFollowed(index)} className={`py-1 px-3 rounded  ${author.isFollowing?'bg-red-500 text-white':'bg-black text-white'} `}>{author.isFollowing?"Unfollow":"Follow"}</button>
+            </li>
+          ))}
+         </ul>
     </div>
   );
 };
